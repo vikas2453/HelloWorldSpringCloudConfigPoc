f2a3b244fa96edaa196b57385822243e14df4333
Vikas Chaudhary <M1052611@mindtree.com>
2019-06-11 16:02:03 -0400

    Adding refresh scope

--- src/main/java/com/mindtree/poc/helloWorld/endpoint/HelloWorldController.java
+++ src/main/java/com/mindtree/poc/helloWorld/endpoint/HelloWorldController.java
@@ -1,29 +1,44 @@
 package com.mindtree.poc.helloWorld.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.mindtree.poc.helloWorld.config.ConfigClientAppConfiguration;
 
+import lombok.Data;
+
+@RestController
+@Data
+@RefreshScope
 public class HelloWorldController {
+	
+	@Autowired
+	private ConfigClientAppConfiguration properties;
+	
+	@Value("${server.instance.name}")
+	private String instance;
+	
 	@Value("${homeMessage}")
 	private String homeMessage;
 	
-	@Value("${greeting}")
+	@Value("${greeting.greetings}")
 	private String greeting;
 	
-	@Value("${name}")
+	@Value("${greeting.name}")
 	private String name;
 	
 	@RequestMapping("/home")
 	public String home() {
-		return homeMessage;
+		return homeMessage+" from "+ instance;
 	}
 	
 	@RequestMapping("/greet")
 	public String greet() {
-		return greeting+name;
+		System.out.println(properties.getUrl());
+		return greeting+" "+name + " from "+ instance+"\n url is "+properties.getUrl();
 	}
 	
 
